cea42bab94dfa570847b2c151ad261d294e72479
Leandro <nanobev@gmail.com>
2023-07-16 22:18:15 -0300

    menu Tesla

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import styled from 'styled-components'
+import Fade from 'react-reveal/Fade';
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImage}) {
   return (
-    <Wrap bgImage = {backgroundImage}>
-      <ItemText>
-        <h1>{title}</h1>
-        <p>{description}</p>
-      </ItemText>
-        <Buttons>
-          <ButtonGroup>
-            <LeftButton>
-              {leftBtnText}
-            </LeftButton>
-            {rightBtnText &&
-              <RightButton>
-                {rightBtnText}
-              </RightButton>
-            }
-          </ButtonGroup>
+    <Wrap bgImage={backgroundImage}>
+       <Fade bottom>
+        <ItemText>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </ItemText>
+       </Fade>  
+      <Buttons>
+          <Fade bottom>
+            <ButtonGroup>
+              <LeftButton>
+                {leftBtnText}
+              </LeftButton>
+              {rightBtnText &&
+                <RightButton>
+                  {rightBtnText}
+                </RightButton>
+              }
+            </ButtonGroup>
+          </Fade>  
           <DownArrow src="/images/down-arrow.svg" />
         </Buttons>
 
@@ -31,6 +36,7 @@ function Section({ title, description, leftBtnText, rightBtnText, backgroundImag
 export default Section
 
 const Wrap = styled.div`
+  z-index: 10;  
   width: 100vw;
   height: 100vh;
   background-size: cover;
